47807ea3b00bf29ef8ebf3b5040cba1773dc820d
Nick-Lemy <n.kayiranga@alustudent.com>
2025-04-19 03:41:05 +0200

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div className='text-4xl'>Home</div>
+    <div className='text-4xl'>
+      <ul>
+        <li>
+          <Link to='/home'>Home</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link to='/register'>Register</Link>
+        </li>
+
+      </ul>
+    </div>
   )
 }
 
