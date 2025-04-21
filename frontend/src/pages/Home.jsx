7dfb4f84e558f54f8ca75905fcf075f81def00a6
Nick-Lemy <n.kayiranga@alustudent.com>
2025-04-21 12:57:13 +0200

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,8 +24,8 @@ function Home() {
   }, [searchTerm ])
 
   return (
-    <div className="flex bg-gray-800 justify-center overflow-auto p-2 md:p-4">
-      <div className="flex-1 p-5 md:p-10 border-2 border-gray-600 bg-gray-900 rounded-2xl">
+    <div className="flex bg-gray-800 justify-center overflow-auto md:p-4">
+      <div className="flex-1 p-5 md:p-10 border-2 m-2 border-gray-600 bg-gray-900 rounded-2xl">
         <Navbar />
         <div className="relative w-fit pb-5">
           <input onChange={(e) => setSearchTerm(e.target.value) } type="text" placeholder="Search..." className="bg-transparent text-white border border-gray-400 rounded-full py-2 px-4 w-64" />
