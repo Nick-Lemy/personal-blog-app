316efebfe3881f04c94007c617172758c952808f
Nick-Lemy <n.kayiranga@alustudent.com>
2025-04-21 15:06:13 +0200

    added markdown text for main corps of posts

--- frontend/src/pages/Post.jsx
+++ frontend/src/pages/Post.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { BACKEND_BASE_URL } from '../utils/environment.varible.mjs';
 import ReactMarkdown from "react-markdown";
+import Navbar from '../sections/Navbar';
 
 function Post() {
     const { id } = useParams()
@@ -21,14 +22,20 @@ function Post() {
         getPost(id)
     }, [id])
     return (
-        <div className='text-white flex flex-col gap-5'>
-            <p>Post {post._id}</p>
-            <h1 className='text-2xl font-bold'>{post.title}</h1>
-            <p>
-                <ReactMarkdown>
-                    {post.main}
-                </ReactMarkdown>
-            </p>
+
+        <div className="flex bg-gray-800 justify-center overflow-auto md:p-4">
+            <div className="flex-1 p-5 md:p-10 border-2 m-2 border-gray-600 bg-gray-900 rounded-2xl">
+                <Navbar />
+                <div className='text-white flex flex-col'>
+                    <h1 className='text-lg font-bold'>{post.title}</h1>
+                    <h2 className='text-xs text-gray-200 font-light'>{post.subtitle}</h2>
+                    <div className='markdown'>
+                        <ReactMarkdown>
+                            {post.main}
+                        </ReactMarkdown>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/sections/Navbar.jsx
+++ frontend/src/sections/Navbar.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
 
-  
-    const tabs = document.querySelectorAll('.navbar button');
-  
-    tabs.forEach(tab => {
-      tab.addEventListener('click', function () {
-        // Remove active class from all tabs
-        tabs.forEach(t => {
-          t.classList.remove('text-white', 'bg-blue-900');
-          t.classList.add('text-gray-400');
-        });
-  
-        // Add active class to clicked tab
-        this.classList.remove('text-gray-400');
-  
-        if (!this.classList.contains('bg-blue-900')) {
-          this.classList.add('text-white', 'bg-blue-900');
-        }
+
+  const tabs = document.querySelectorAll('.navbar *');
+
+  tabs.forEach(tab => {
+    tab.addEventListener('click', function () {
+      // Remove active class from all tabs
+      tabs.forEach(t => {
+        t.classList.remove('text-white', 'bg-blue-900');
+        t.classList.add('text-gray-400');
       });
+
+      // Add active class to clicked tab
+      this.classList.remove('text-gray-400');
+
+      if (!this.classList.contains('bg-blue-900')) {
+        this.classList.add('text-white', 'bg-blue-900');
+      }
     });
+  });
   return (
     <>
       <div className="flex justify-between items-center mb-8">
@@ -36,11 +37,21 @@ function Navbar() {
       </div>
 
       {/* <!-- Tabs --> */}
-      <div className="navbar flex mb-2 *:text-sm *:md:text-base *:cursor-pointer *:transform *:duration-200 *:rounded-md *:ease-in">
-        <button className="px-4 py-2 text-white bg-blue-900">Home</button>
-        <button className="px-4 py-2 text-gray-400">Favorites</button>
-        <button className="px-4 py-2 text-gray-400">History</button>
-        <button className="px-4 py-2 text-gray-400">About</button>
+      <div className="navbar flex mb-2 *:text-sm **:md:text-base **:cursor-pointer *:transform *:duration-200 *:rounded-md *:ease-in">
+        <Link to='/' className="px-4 py-2 text-white bg-blue-900">
+          Home
+        </Link>
+        <Link to='/favorites' className="px-4 py-2 text-gray-400">
+          Favorites
+        </Link>
+
+        <Link to='/history' className="px-4 py-2 text-gray-400">
+          History
+        </Link>
+
+        <Link to='/about' className="px-4 py-2 text-gray-400">
+          About
+        </Link>
       </div>
 
       <hr className="border-gray-800 border-1 mb-6" /></>
