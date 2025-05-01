b86b9926d549489ed9b6748305e393621fcd7d1f
Nick-Lemy <n.kayiranga@alustudent.com>
2025-05-01 16:00:29 +0200

    upload jwt auth verification

--- backend/middlewares/auth.middleware.mjs
+++ backend/middlewares/auth.middleware.mjs
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { TOKEN_SECRET } from '../utils/constants.mjs';
 
 export const verifyToken = (req, res, next)=>{
-    const token = req.header('Authorization')
+    const token = req.header('Authorization').split(' ')[1]
     if(!token) return res.status(401).send({ error: 'Access denied' })
     try {
         const decode = jwt.verify(token, TOKEN_SECRET)
