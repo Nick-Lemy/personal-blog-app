5866ae3b710a0f5d3c3344e7b29cb1d905c46ffd
Nick-Lemy <n.kayiranga@alustudent.com>
2025-05-02 22:43:20 +0200

    prevent default behaviors of divs

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -18,7 +18,12 @@ const Account = () => {
         <div className="flex bg-gray-800 justify-center overflow-auto md:p-4">
             <div className="flex-1 p-5 md:p-10 border-2 m-2 border-gray-600 bg-gray-900 rounded-2xl">
                 <div>
-                    <div onClick={() => navigate(-1)}>
+                    <div onClick={
+                        (e) => {
+                            e.preventDefault()
+                            navigate(-1)
+                        }
+                        }>
                         <svg className='cursor-pointer w-6 md:w-10 mb-5 fill-white' id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 108.06">
                             <title>back-arrow</title>
                             <path d="M63.94,24.28a14.28,14.28,0,0,0-20.36-20L4.1,44.42a14.27,14.27,0,0,0,0,20l38.69,39.35a14.27,14.27,0,0,0,20.35-20L48.06,68.41l60.66-.29a14.27,14.27,0,1,0-.23-28.54l-59.85.28,15.3-15.58Z" />
