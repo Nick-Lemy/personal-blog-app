fea47738344e85b9eae465b97e23f3c2d8358a3a
Nick-Lemy <n.kayiranga@alustudent.com>
2025-05-02 19:05:20 +0200

    update  ai summary

--- frontend/src/pages/Post.jsx
+++ frontend/src/pages/Post.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { BACKEND_BASE_URL, GEMINI_API_KEY } from '../utils/environment.varible.mjs';
-import { Link } from 'react-router-dom';
 import MarkdownPreview from '@uiw/react-markdown-preview';
 import Like from '../componets/Like';
 import axios from 'axios';
@@ -34,7 +33,8 @@ function Post() {
 
         const response = await axios.post(URL, body)
         const data = response.data
-        alert(data['candidates'][0]['content']['parts'][0].text)
+        const res = data['candidates'][0]['content']['parts'][0].text
+        alert(res)
     }
 
     const addToFav = async (id) => {
@@ -91,7 +91,8 @@ function Post() {
     }, [id])
     return (
 
-        <div className="flex bg-gray-800 justify-center overflow-auto md:p-4">
+        <div id='corps' className="relative flex bg-gray-800 justify-center overflow-auto md:p-4">
+
             <div className="flex-1 p-5 md:p-10 border-2 m-2 border-gray-600 bg-gray-900 rounded-2xl">
                 <div>
                     <div onClick={() => navigate(-1)}>
@@ -136,7 +137,7 @@ function Post() {
                                 <p>Favorites</p>
                             </div>
                             <div onClick={
-                                ()=>getAISummary(post.main)
+                                () => getAISummary(post.main)
                             } className='cursor-pointer flex flex-col h-fit justify-center items-center w-fit'>
                                 <svg className=' fill-white' xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24"><path d="M11 6.999c2.395.731 4.27 2.607 4.999 5.001.733-2.395 2.608-4.269 5.001-5-2.393-.731-4.268-2.605-5.001-5-.729 2.394-2.604 4.268-4.999 4.999zm7 7c1.437.438 2.562 1.564 2.999 3.001.44-1.437 1.565-2.562 3.001-3-1.436-.439-2.561-1.563-3.001-3-.437 1.436-1.562 2.561-2.999 2.999zm-6 5.501c1.198.365 2.135 1.303 2.499 2.5.366-1.198 1.304-2.135 2.501-2.5-1.197-.366-2.134-1.302-2.501-2.5-.364 1.197-1.301 2.134-2.499 2.5zm-6-8.272c.522.658 1.118 1.253 1.775 1.775-.657.522-1.252 1.117-1.773 1.774-.522-.658-1.118-1.253-1.776-1.776.658-.521 1.252-1.116 1.774-1.773zm-.001-4.228c-.875 2.873-3.128 5.125-5.999 6.001 2.876.88 5.124 3.128 6.004 6.004.875-2.874 3.128-5.124 5.996-6.004-2.868-.874-5.121-3.127-6.001-6.001z" /></svg>
                                 <p>AI Summary</p>
